5f00981af9e420b8efc3e9cefcac3e4b2073c539
TSP66 <62830753+TSP66@users.noreply.github.com>
2021-05-14 10:40:27 +1000

    Continue wallet programming

--- wallet.c
+++ wallet.c
@@ -3,7 +3,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(){
-  return(0);
+    if((access("public_key.txt", F_OK ) == 0) && (access("private_key.txt", F_OK ) == 0)) {
+        // file exists
+    } else {
+        system("python3 ccc.py");
+    }
+    return(0);
 }
